63157f3ed6c5f5767f2c6ce7c5c4230d2783c1c0
lalitdutt <lalitparsai@gmail.com>
2017-03-12 15:48:50 +0530

    Add Comments + Message format change

--- CustomPartitionKafka/src/main/scala-2.12/KafkaLearning/CustomPartitioner.scala
+++ CustomPartitionKafka/src/main/scala-2.12/KafkaLearning/CustomPartitioner.scala
@@ -1,19 +1,16 @@
 package KafkaLearning
-
-import java.util
 import java.util.Properties
-
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer._
-
 /**
   * Created by LALITDUTT on 22/2/17.
   */
 
 object CustomPartitioner{
-
+  /*To Execute Program*/
   def main(args: Array[String]): Unit = {
+    //Single Broker
     val brokers = "localhost:9092"
+    //Kafka Topic With 5 partition
     val topic = "productdata"
     val productDataProducer=new ProductDataProducer()
     val producerProp=productDataProducer.getProducerProperties(brokers)
@@ -35,16 +32,15 @@ object CustomPartitioner{
     def sendMessage(topic:String,props:Properties):Unit={
       val productPartitionService=new ProductSegmentPartitionService()
       val producerUser=new KafkaProducer[String,String](props)
-      for(product<-productPartitionService.getAllUser()){
-        val msg="Product "+product._1
+      for(product<-productPartitionService.getAllSegment()){
+        val msg="""{"data":{"product_name":"dummy product title","segment":""""+product._1+""""}}"""
+        println(msg)
         val record = new ProducerRecord[String, String](topic,product._1, msg)
         producerUser.send(record)
       }
       producerUser.close()
     }
   }
-
-
 }
 
 
--- CustomPartitionKafka/src/main/scala-2.12/KafkaLearning/KafkaProductSegmentCustomPartition.scala
+++ CustomPartitionKafka/src/main/scala-2.12/KafkaLearning/KafkaProductSegmentCustomPartition.scala
@@ -3,10 +3,10 @@ package KafkaLearning
 import java.util
 import org.apache.kafka.clients.producer.Partitioner
 import org.apache.kafka.common.Cluster
-
 /**
   * Created by Lalitdutt on 23/2/17.
-  */
+  **/
+
 /*Custom Partition Implementation */
 class KafkaProductSegmentCustomPartition extends Partitioner{
   val productSegmentPartitionService=new ProductSegmentPartitionService()
@@ -22,7 +22,7 @@ class KafkaProductSegmentCustomPartition extends Partitioner{
   }
 }
 
-
+/*Product Segment Master Model*/
 class ProductSegmentPartitionService {
   var segmentData=collection.mutable.Map[String, Int]()
 
@@ -37,7 +37,7 @@ class ProductSegmentPartitionService {
   def getID(name:String): Int ={
     return segmentData.get(name).get
   }
-  def getAllUser():collection.mutable.Map[String,Int]={
+  def getAllSegment():collection.mutable.Map[String,Int]={
     return segmentData
   }
 }
